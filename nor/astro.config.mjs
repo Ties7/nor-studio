e7ecee65d9898e4000365ec0cb8ec63d9573a546
Ties7 <97968332+Ties7@users.noreply.github.com>
2026-06-01 12:05:05 +0200

    feat: sanity + astro installatie #7

--- nor/astro.config.mjs
+++ nor/astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config'
-import sanity from '@sanity/astro'
-
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [sanity({
-    projectId: 'etjm03yk',
-    dataset: 'production',
-    studioBasePath: '/studio',
-  }), react()],
-})
+  integrations: [
+    sanity({
+      projectId: "etjm03yk",
+      dataset: "production",
+      studioBasePath: "/studio",
+    }),
+    react(),
+  ],
+});
--- nor/src/lib/sanity.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@sanity/client'
-
-export const sanityClient = createClient({
-  projectId: 'etjm03yk',
-  dataset: 'production',
-  apiVersion: '2024-01-01',
-  useCdn: true,
-})

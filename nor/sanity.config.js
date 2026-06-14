abf12f926ceeef176f6ad62e5ec41b854e8e99e8
Ties7 <97968332+Ties7@users.noreply.github.com>
2026-06-14 20:43:06 +0200

    .

--- nor/sanity.config.js
+++ nor/sanity.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import product from '../studio/schemaTypes/product.js'
 
 export default defineConfig({
   projectId: 'etjm03yk',
   dataset: 'production',
   plugins: [structureTool()],
   schema: {
-    types: [product],
+    types: [],
   },
 })
